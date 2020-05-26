f3e74c39ca161719bfec8bbf0e0656cd2a847956
jonathanim <imjonathan.work@gmail.com>
2020-05-26 02:12:42 -0700

    added bootstrap and work on landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   resources :jobs
   resources :job_applications
-  root to: "jobs#index"
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
